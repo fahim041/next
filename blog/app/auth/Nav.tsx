eacc9046b42741de3be7848b3156f010cb71a7a7
M0nster92 <fahimalsafi@gmail.com>
2023-02-20 15:46:41 -0700

    server side authentication working

--- blog/app/auth/Nav.tsx
+++ blog/app/auth/Nav.tsx
@@ -1,19 +1,21 @@
-"use client";
 import Link from "next/link";
-import { useEffect } from "react";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
+import Login from "./Login";
+import Logged from "./Logged";
 
-export default function Nav() {
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
-
+export default async function Nav() {
+  const session = await getServerSession(authOptions);
+  // console.log(session);
   return (
-    <nav>
+    <nav className="flex justify-between items-center py-8">
       <Link href={"/"}>
-        <h1>Send it.</h1>
+        <h1 className="font-bold text-lg">Send it.</h1>
       </Link>
+      <ul className="flex items-center gap-6">
+        {!session?.user && <Login />}
+        {session?.user && <Logged image={session.user?.image || ""} />}
+      </ul>
     </nav>
   );
 }
--- blog/app/layout.jsx
+++ blog/app/layout.jsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
-      <body className="mx-4 md:mx-48 xl:mx-96">
+      <body className={`mx-4 md:mx-48 xl:mx-96 ${roboto.variable}`}>
         <Nav />
         {children}
       </body>
