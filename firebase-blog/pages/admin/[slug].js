a571bfd005320c4f75a1f36e8d5d6177b7d4ad14
Fahim090 <FAlSami@grantmatch.com>
2023-03-31 11:34:14 -0600

    nav, spinner & router

---
+++ firebase-blog/pages/admin/[slug].js
@@ -0,0 +1,7 @@
+export default function AdminPostEdit({}) {
+  return (
+    <main>
+      <h1>Admin Post Edit Page</h1>
+    </main>
+  );
+}
---
+++ firebase-blog/pages/enter.js
@@ -0,0 +1,22 @@
+export default function EnterPage() {
+  const user = null;
+  return (
+    <main>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
+    </main>
+  );
+}
+
+function SignInButton() {}
+
+function SignOutButton() {}
+
+function UsernameForm() {}
