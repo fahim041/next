6d074b507eefe0e1450e57e6da43072b10f4b53e
Fahim090 <FAlSami@grantmatch.com>
2023-03-31 19:24:13 -0600

    SSR: user profile page

--- firebase-blog/lib/firebase.js
+++ firebase-blog/lib/firebase.js
@@ -20,3 +20,19 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();
 export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export async function getUserWithUsername(username) {
+  const usersRef = firestore.collection("users");
+  const query = usersRef.where("username", "==", username).limit(1);
+  const userDoc = (await query.get()).docs[0];
+  return userDoc;
+}
+
+export function postToJSON(doc) {
+  const data = doc.data();
+  return {
+    ...data,
+    createdAt: data?.createdAt.toMillis() || 0,
+    updatedAt: data?.updatedAt.toMillis() || 0,
+  };
+}
